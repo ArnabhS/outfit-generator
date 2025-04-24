3329d0c8b8057803062f1fedf507851ff87f4ca5
ArnabhS <sinhaarnabh888@gmail.com>
2025-04-24 23:41:04 +0530

    Refactor code structure for improved readability and maintainability

--- server/src/routes/generatror.routes.js
+++ server/src/routes/generatror.routes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import multer from "multer"
+
 import { generateImage } from "../controllers/generateImage.controller.js";
+import upload from "../middleware/multer.js";
+
 
 const router = express.Router();
 
-const upload = multer({ dest: "/uploads" });
 
 router.post('/',upload.single("image"),generateImage)
 
